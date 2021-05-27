f25a198da0c494bd1321bb4d13f00c709832dd34
LEONID <312agapov@gmail.com>
2021-05-27 21:19:16 +0300

    cmt

---
+++ src/main/java/com/example/rgrgit/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.rgrgit.repository;
+
+import com.example.rgrgit.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
