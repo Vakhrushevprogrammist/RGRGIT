d3cf5e8f7156d2627ffc2bfcb3457739fef28a43
Vakhrushevprogrammist <blizzardteam@mail.ru>
2021-05-27 17:17:24 +0400

    Initial commit

---
+++ src/main/java/com/example/rgrgit/RgrgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.rgrgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgrgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RgrgitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rgrgit/RgrgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rgrgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgrgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
