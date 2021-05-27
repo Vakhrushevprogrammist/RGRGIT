348f6387bb0d54fabe71c8c64b4f4ac2f013928a
dudyaosuplayer <sasch2010pavlov@mail.ru>
2021-05-27 21:22:53 +0300

    commit

---
+++ src/main/java/com/example/rgrgit/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.rgrgit.repository;
+
+import com.example.rgrgit.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
