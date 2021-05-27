6eac7bb6ffd4349eee2c0d5c06aca6f880e8ccac
dudyaosuplayer <sasch2010pavlov@mail.ru>
2021-05-27 21:20:46 +0300

    commit

--- src/main/java/com/example/rgrgit/repository/NotificationRepository.java
+++ src/main/java/com/example/rgrgit/repository/NotificationRepository.java
@@ -1,6 +1,6 @@
 package com.example.rgrgit.repository;
 
-import com.example.Daily.model.Notification;
+import com.example.rgrgit.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
