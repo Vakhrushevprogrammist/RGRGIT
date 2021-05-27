7145ae042a37a9d57c66a6beae91860ac9eb1e5f
LEONID <312agapov@gmail.com>
2021-05-27 17:21:13 +0300

    test

--- src/main/java/com/example/rgrgit/RgrgitApplication.java
+++ src/main/java/com/example/rgrgit/RgrgitApplication.java
@@ -8,6 +8,6 @@ public class RgrgitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RgrgitApplication.class, args);
+        System.out.println("test");
     }
-
 }
